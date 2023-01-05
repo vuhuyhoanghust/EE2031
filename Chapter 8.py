84d0259b207e59d944d011fc5ae4fe26b519539f
Vũ Huy Hoàng <vuhuyhoang.hust@gmail.com>
2023-01-05 13:58:59 +0700

    Update chapter 8

---
+++ Chapter 8.py
@@ -0,0 +1,12 @@
+# chapter 8: Laplacian - Possion equation
+import numpy as np
+from numpy import array, dot, pi
+from numpy import linalg as LA
+from sympy import symbols
+from sympy import solve
+from scipy import integrate
+import math
+from numpy.linalg import inv
+
+print ('Exercise 1 - 2 - 3: Document\n')
+
